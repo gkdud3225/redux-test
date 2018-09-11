06c55db008972ea5bbaf25daf311d62d8c74f558
hayeong <gkdud3225@hanmail.net>
2018-09-11 17:55:22 +0900

    first commit. reducer, store 생성

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,2 +1,13 @@
-// 스토어를 생성하는 함수륾 만들어서 내보냄
+// 스토어를 생성하는 함수를 만들어서 내보냄
 // 이 함수는 store/index.js 에서 불러와서 사용하게됨
+import {createStore} from 'redux';
+import modules from './modules';
+
+const configure = () => {
+    // const store = createStore(modules);
+    const devTools = window.__REDEX_DEVTOOLS_EXTENSION__ && window.__REDEX_DEVTOOLS_EXTENSION__()
+    const store = createStore(modules, devTools);
+    return store;
+}
+
+export default configure;
--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,1 +1,34 @@
 // 카운터 관련 상태 로직
+// action과 reducer를 하나의 파일에 모두 작성하는 것을 Ducks구조라고 한다. 이 구조에서는, 리덕스 관련코드를 기능별로 하나의 파일에 작성한다.
+import {createAction, handleActions} from 'redux-actions';
+
+const INCREMENT = 'counter/INCREMENT'; // 도메인/액션이름
+const DECREMENT = 'counter/DECREMENT';
+
+// export const increment = () => ({type: INCREMENT});
+// export const decrement = () => ({type: DECREMENT});
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
+
+
+const initialState = {
+    number: 0
+}
+// export default function reducer(state = initialState, action) {
+//     switch(action.type) {
+//         case INCREMENT:
+//             return {number: state.number + 1};
+//         case DECREMENT:
+//             return {number: state.number - 1};
+//         default:
+//             return state;
+//     }
+// }
+
+export default handleActions({
+    [INCREMENT]: (state, action) => {
+        return {number: state.number +1};
+    },
+    // action 객체를 참조하지 않으므로, stste부분에서 비구조화 할당도 해주면 다음과 같이 코드를 간소화시킬 수 있다.
+    [DECREMENT]: ({number}) => ({number: number - 1})
+}, initialState)
